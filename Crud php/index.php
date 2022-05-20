b8cdcd7adca738f12ad241bbf87286cf705da131
DiegoValMu <diego.valderrama.mu@gmail.com>
2022-05-20 14:58:26 -0400

    Crud con Php

---
+++ Crud php/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ayudantia</title>
+</head>
+
+<body style="display:flex;justify-content:center;align-items:center;flex-direction:column;  margin:0">
+    <div>
+        <h1 style="">CRUD</h1>
+    </div>
+    <div>
+        <a href="./Crear/agregar.view.php">Crear</a>
+        <a href="./eliminar y modificar Php/index.php">Leer</a>
+    </div>
+</body>
+
+</html>
